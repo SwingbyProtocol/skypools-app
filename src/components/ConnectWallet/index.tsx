0125fb2b36cffac6665fae07e0141a477e42d2fb
Rubén <ruben@swingby.network>
2021-06-16 11:24:19 +0200

    fix: Hide health indicator if no wallet is connected

--- src/components/ConnectWallet/index.tsx
+++ src/components/ConnectWallet/index.tsx
@@ -4,7 +4,7 @@ import { shortenAddress } from '../../modules/short-address';
 import { logger } from '../../modules/logger';
 import { Button } from '../Button';
 import { NetworkTag } from '../NetworkTag';
-import { useOnboard } from '../../modules/onboard';
+import { isValidNetworkId, useOnboard } from '../../modules/onboard';
 import { useHealthCheck } from '../../modules/1inch';
 
 import { container, networkTag, walletWrapper, healthIndicator, healthy } from './styled';
@@ -16,7 +16,9 @@ export const ConnectWallet = ({ className }: { className?: string }) => {
   return (
     <div css={container} className={className}>
       <div css={walletWrapper}>
-        <div css={[healthIndicator, isHealthy && healthy]} />
+        {!!address && isValidNetworkId(network) && (
+          <div css={[healthIndicator, isHealthy && healthy]} />
+        )}
         {!!address && <NetworkTag css={networkTag} network={network} />}
         <Button
           variant="secondary"
