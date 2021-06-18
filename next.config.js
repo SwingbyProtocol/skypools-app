1c93495cc4ab99beb87b5a8fd186e890b0f43253
Rubén <ruben@swingby.network>
2021-06-18 12:47:59 +0200

    chore: Upgraded to Next.js 11

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 module.exports = {
+  webpack5: true,
   basePath: process.env.NEXT_CONFIG_BASE_PATH || undefined,
   i18n: {
     locales: ['en', 'es', 'ja', 'zh', 'zh-TW'],
     defaultLocale: 'en',
-  },
-  future: {
-    webpack5: true,
   },
   images: {
     domains: ['img.paraswap.network', 'raw.githubusercontent.com', 'tokens.1inch.exchange'],
