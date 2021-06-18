20605135402121df46d2d9646dd0b833eb49836d
Rubén <ruben@swingby.network>
2021-06-18 12:42:26 +0200

    refactor: Simplified `Coin` component

--- src/components/Coin/index.tsx
+++ src/components/Coin/index.tsx
@@ -1,5 +1,4 @@
 import NextImage from 'next/image';
-import { useEffect, useState } from 'react';
 
 import { coin } from './styles';
 
@@ -12,36 +11,9 @@ export const Coin = ({
   src: string | null | undefined;
   className?: string;
 }) => {
-  const [src, setSrc] = useState<string>(UNKNOWN);
-
-  useEffect(() => {
-    if (!srcParam) {
-      return;
-    }
-
-    let cancelled = false;
-
-    const img = new Image();
-    img.onload = () => {
-      if (cancelled) return;
-      setSrc(srcParam);
-    };
-
-    img.onerror = () => {
-      if (cancelled) return;
-      setSrc(UNKNOWN);
-    };
-
-    img.src = srcParam;
-
-    return () => {
-      cancelled = true;
-    };
-  }, [srcParam]);
-
   return (
     <div css={coin} className={className}>
-      <NextImage src={src} layout="fill" />
+      <NextImage src={srcParam || UNKNOWN} layout="fill" />
     </div>
   );
 };
