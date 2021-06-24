75ade97f082f9d481db8e23df92680f82c78bf8f
Rubén <ruben@swingby.network>
2021-06-24 12:20:25 +0200

    fix: Logo in dark mode

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { useMedia } from 'react-use';
 
 import { ConnectWallet } from '../../components/ConnectWallet';
 
@@ -7,10 +8,11 @@ import { header, logo, connect } from './styles';
 type Props = { className?: string };
 
 export const Header = ({ className }: Props) => {
+  const isDarkMode = useMedia('(prefers-color-scheme: dark)', false);
   return (
     <header css={header} className={className}>
       <div css={logo}>
-        <Image src="/logo.svg" alt="Skypools" layout="fill" />
+        <Image src={`/logo-${isDarkMode ? 'dark' : 'light'}.svg`} alt="Skypools" layout="fill" />
       </div>
       <ConnectWallet css={connect} />
     </header>
