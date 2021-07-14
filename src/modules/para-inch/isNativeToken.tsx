83967a284a894d602961f763499e7a6114bd189e
Rubén <ruben@swingby.network>
2021-07-14 15:17:43 +0200

    refactor: Reg exp used in `isNativeToken`

--- src/modules/para-inch/isNativeToken.tsx
+++ src/modules/para-inch/isNativeToken.tsx
@@ -1,5 +1,5 @@
 export const NATIVE_TOKEN_ADDRESS = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
 
 export function isNativeToken(value: string): boolean {
-  return /^0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee$/i.test(value);
+  return new RegExp(`^${NATIVE_TOKEN_ADDRESS}$`, 'i').test(value);
 }
