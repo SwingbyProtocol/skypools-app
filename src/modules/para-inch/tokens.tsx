3d02191144b3c93d28e8f48effaec3f456822148
Rubén <ruben@swingby.network>
2021-07-26 13:18:09 +0200

    feat: Get coin logo from Coingecko only if we do not already have one

--- src/modules/para-inch/tokens.tsx
+++ src/modules/para-inch/tokens.tsx
@@ -3,6 +3,7 @@ import { ParaSwap } from 'paraswap';
 import { shouldUseParaSwap } from '../env';
 import { fetcher } from '../fetch';
 import { Network, getNetworkId, getNetwork } from '../onboard';
+import { prisma } from '../server__env';
 
 import { getCoinLogo } from './coin-details';
 import { ENDPOINT_1INCH_API } from './constants';
@@ -17,6 +18,8 @@ export type ParaInchToken = {
 };
 
 export const getTokens = async ({ network }: { network: Network }): Promise<ParaInchToken[]> => {
+  const dbTokens = await prisma.token.findMany({ where: { network } });
+
   if (shouldUseParaSwap) {
     const paraSwap = new ParaSwap(getNetworkId(network));
     const tokens = await paraSwap.getTokens();
@@ -26,17 +29,22 @@ export const getTokens = async ({ network }: { network: Network }): Promise<Para
 
     return (
       await Promise.all(
-        tokens.map(
-          async (it): Promise<ParaInchToken> => ({
+        tokens.map(async (it): Promise<ParaInchToken> => {
+          const dbToken = dbTokens.find(
+            ({ address }) => address.toLowerCase() === it.address.toLowerCase(),
+          );
+
+          return {
             symbol: it.symbol ?? '',
             decimals: +it.decimals,
             address: it.address,
             logoUri:
               ((it.img === 'https://img.paraswap.network/token.png' ? null : it.img) || null) ??
+              dbToken?.logoUri ??
               (await getCoinLogo({ network, tokenAddress: it.address })),
             network: getNetwork(it.network)!,
-          }),
-        ),
+          };
+        }),
       )
     ).filter((it) => !!it.symbol && !!it.address);
   }
@@ -48,13 +56,22 @@ export const getTokens = async ({ network }: { network: Network }): Promise<Para
     >;
   }>(`${ENDPOINT_1INCH_API}/${network}/tokens`);
 
-  return Object.values(result.tokens).map(
-    (it): ParaInchToken => ({
-      symbol: it.symbol,
-      decimals: +it.decimals,
-      address: it.address,
-      logoUri: it.logoURI || null,
-      network,
+  return await Promise.all(
+    Object.values(result.tokens).map(async (it): Promise<ParaInchToken> => {
+      const dbToken = dbTokens.find(
+        ({ address }) => address.toLowerCase() === it.address.toLowerCase(),
+      );
+
+      return {
+        symbol: it.symbol,
+        decimals: +it.decimals,
+        address: it.address,
+        logoUri:
+          (it.logoURI || null) ??
+          dbToken?.logoUri ??
+          (await getCoinLogo({ network, tokenAddress: it.address })),
+        network,
+      };
     }),
   );
 };
