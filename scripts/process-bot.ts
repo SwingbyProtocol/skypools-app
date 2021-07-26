d3c6efdfa912b83e3be0ffac7a83dc829167135d
Rubén <ruben@swingby.network>
2021-07-26 13:12:51 +0200

    chore: Update tokens only every 2 hours

--- scripts/process-bot.ts
+++ scripts/process-bot.ts
@@ -8,8 +8,17 @@ import { server__processTaskSecret } from '../src/modules/server__env';
 const REPEAT_INTERVAL = Duration.fromObject({ seconds: 30 }).as('milliseconds');
 const TIMEOUT_AFTER = Duration.fromObject({ minutes: 1.5 }).as('milliseconds');
 
+type TaskConfig = { name: string; repeatInterval?: number };
+type Task = string | TaskConfig;
+
+const isTaskConfig = (value: any): value is TaskConfig =>
+  typeof value === 'object' && typeof value.name === 'string';
+
 const NETWORKS = Object.values(Network).map((it) => it.toLowerCase() as Lowercase<Network>);
-const NETWORK_TASKS = ['tokens', 'prices-historic'];
+const NETWORK_TASKS: Task[] = [
+  { name: 'tokens', repeatInterval: Duration.fromObject({ hours: 2 }).as('milliseconds') },
+  'prices-historic',
+];
 
 NETWORKS.forEach((mode) => {
   NETWORK_TASKS.forEach((task) => {
@@ -27,12 +36,17 @@ async function* runNetworkTask(
   task: typeof NETWORK_TASKS[number],
 ) {
   for (;;) {
+    const config: TaskConfig = {
+      repeatInterval: REPEAT_INTERVAL,
+      ...(isTaskConfig(task) ? task : { name: task }),
+    };
+
     try {
       const controller = new AbortController();
 
       const id = setTimeout(() => controller.abort(), TIMEOUT_AFTER);
       const result = await fetcher<Record<string, any>>(
-        `https://k8s.skybridge.exchange/skypools/api/process/${network}/${task}?secret=${server__processTaskSecret}`,
+        `https://k8s.skybridge.exchange/skypools/api/process/${network}/${config.name}?secret=${server__processTaskSecret}`,
         { signal: controller.signal },
       );
       clearTimeout(id);
@@ -42,6 +56,6 @@ async function* runNetworkTask(
       yield { mode: network, task, err: e as Error };
     }
 
-    await new Promise((resolve) => setTimeout(resolve, REPEAT_INTERVAL));
+    await new Promise((resolve) => setTimeout(resolve, config.repeatInterval));
   }
 }
