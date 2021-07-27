e4c98090c79b3c026bd7eab6058620f955e44f9d
Rubén <ruben@swingby.network>
2021-07-27 16:39:26 +0200

    fix: `fromGraphWhereArgToPrisma` keeps `null` value for `equals` prop

--- src/modules/server__gql-model/fromGraphWhereArgToPrisma.ts
+++ src/modules/server__gql-model/fromGraphWhereArgToPrisma.ts
@@ -2,7 +2,14 @@ import { Prisma } from '@prisma/client';
 import { DateTime } from 'luxon';
 import lodashMapValues from 'lodash.mapvalues';
 
-export const fromGraphWhereArgToPrisma = <T extends unknown>(param: T): any => {
+const isObject = (value: unknown): value is object =>
+  !Array.isArray(value) && typeof value === 'object';
+
+export const fromGraphWhereArgToPrisma = <T extends unknown>(param: T, propName?: string): any => {
+  if (param === null && propName === 'equals') {
+    return null;
+  }
+
   if (typeof param === 'undefined' || param === null) {
     return undefined;
   }
@@ -18,9 +25,6 @@ export const fromGraphWhereArgToPrisma = <T extends unknown>(param: T): any => {
   if (Array.isArray(param)) {
     return param.map((it) => fromGraphWhereArgToPrisma(it));
   }
-
-  const isObject = (value: unknown): value is object =>
-    !Array.isArray(value) && typeof value === 'object';
 
   if (isObject(param)) {
     return lodashMapValues(param, fromGraphWhereArgToPrisma);
