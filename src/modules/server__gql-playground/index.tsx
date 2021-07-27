a1b22c7e8a9d6111d2513c852483aa100ff172f3
Rubén <ruben@swingby.network>
2021-07-27 17:08:31 +0200

    feat: Added example query to playground

--- src/modules/server__gql-playground/index.tsx
+++ src/modules/server__gql-playground/index.tsx
@@ -1,8 +1,32 @@
+import { parse, print } from 'graphql';
 import type { Config } from 'apollo-server-micro';
 
 import { graphqlEndpoint, selfUrl } from '../env';
 
+const endpoint = `${selfUrl}${graphqlEndpoint}`;
+
 export const playground: Config['playground'] = {
-  endpoint: `${selfUrl}${graphqlEndpoint}`,
-  tabs: undefined,
+  endpoint,
+  tabs: [
+    {
+      name: 'Ethereum tokens',
+      endpoint,
+      query: print(
+        parse(`
+          {
+            tokens(where: { network: { equals: ETHEREUM } }) {
+              totalCount
+              edges {
+                node {
+                  address
+                  symbol
+                  decimals
+                }
+              }
+            }
+          }
+      `),
+      ),
+    },
+  ],
 };
