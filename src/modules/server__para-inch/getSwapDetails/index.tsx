cc4de9da9596b9e28fd2151698883c92d39fbbf8
Rubén <ruben@swingby.network>
2021-07-30 19:10:03 +0200

    chore: Cleanup types

--- src/modules/server__para-inch/getSwapDetails/index.tsx
+++ src/modules/server__para-inch/getSwapDetails/index.tsx
@@ -181,28 +181,11 @@ const getToAmountFromScan = async ({
   }
 
   type ApiResult = {
-    status: string;
-    message: string;
     result: Array<{
-      blockNumber: string;
-      timeStamp: string;
       hash: string;
-      nonce: string;
-      blockHash: string;
-      from: string;
-      contractAddress: string;
       to: string;
       value: string;
-      tokenName: string;
-      tokenSymbol: string;
       tokenDecimal: string;
-      transactionIndex: string;
-      gas: string;
-      gasPrice: string;
-      gasUsed: string;
-      cumulativeGasUsed: string;
-      input: string;
-      confirmations: string;
     }>;
   };
 };
