81cdf95ac97fd12a18711a831705b9942d1b02ff
roy1210 <rswkure@gmail.com>
2021-11-19 19:24:54 +0800

    fix: clean up

--- src/modules/skypools/useSkybridgeSwap.tsx
+++ src/modules/skypools/useSkybridgeSwap.tsx
@@ -3,7 +3,7 @@ import { useEffect, useMemo, useState } from 'react';
 import { useSkybridgeSwapInfoLazyQuery } from '../../generated/skybridge-graphql';
 
 export const useSkybridgeSwap = (skybridgeId: string) => {
-  const [wbtcSrcAmount, setwbtcSrcAmount] = useState<string>('0');
+  const [wbtcSrcAmount, setWbtcSrcAmount] = useState<string>('0');
   const [status, setStatus] = useState<string>('WAITING');
 
   const [getSwaps, result] = useSkybridgeSwapInfoLazyQuery();
@@ -20,10 +20,9 @@ export const useSkybridgeSwap = (skybridgeId: string) => {
 
   useEffect(() => {
     if (!result.data) return;
-    console.log('result', result.data);
 
     setStatus(result.data.transaction.status);
-    setwbtcSrcAmount(result.data.transaction.receivingAmount);
+    setWbtcSrcAmount(result.data.transaction.receivingAmount);
   }, [result]);
 
   return useMemo(() => {
