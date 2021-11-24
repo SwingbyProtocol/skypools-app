ca06605dc75f0048ef362126e1a75de854008496
roy1210 <rswkure@gmail.com>
2021-11-24 14:51:46 +0800

    fix: add WBTC (ROPSTEN) in db

--- src/modules/server__para-inch/index.tsx
+++ src/modules/server__para-inch/index.tsx
@@ -2,3 +2,4 @@ export { getTokens } from './getTokens';
 export { getSwapQuote } from './getSwapQuote';
 export { getPriceHistoryFromCoingecko, getTokenLogoFromCoingecko } from './coin-details';
 export { getSpender } from './getSpender';
+export { buildTokenId } from './getTokens';
