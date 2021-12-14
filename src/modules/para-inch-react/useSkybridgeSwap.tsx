bd7d75bfa196bf656a2c981d1d600b15c2f88b37
roy1210 <rswkure@gmail.com>
2021-12-14 13:28:10 +0800

    fix: wip: update skybridge status

--- src/modules/para-inch-react/useSkybridgeSwap.tsx
+++ src/modules/para-inch-react/useSkybridgeSwap.tsx
@@ -1,13 +1,16 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState, useCallback } from 'react';
 
 import { useSkybridgeSwapInfoLazyQuery } from '../../generated/skybridge-graphql';
 
 export const useSkybridgeSwap = (skybridgeId: string) => {
   const [wbtcSrcAmount, setWbtcSrcAmount] = useState<string>('0');
   const [status, setStatus] = useState<string>('WAITING');
-  const [getSwaps, result] = useSkybridgeSwapInfoLazyQuery();
+  const [getSwaps, result] = useSkybridgeSwapInfoLazyQuery({
+    pollInterval: 5000,
+    onCompleted: () => console.log('check'),
+  });
 
-  useEffect(() => {
+  const getSwap = useCallback(() => {
     if (!skybridgeId) return;
 
     getSwaps({
@@ -18,8 +21,18 @@ export const useSkybridgeSwap = (skybridgeId: string) => {
   }, [getSwaps, skybridgeId]);
 
   useEffect(() => {
+    getSwap();
+    // const interval = setInterval(() => {
+    //   getSwap();
+    // }, 120000);
+
+    // return () => clearInterval(interval);
+  }, [getSwap]);
+
+  useEffect(() => {
     if (!result.data) return;
 
+    console.log('result.data.transaction.status', result.data.transaction.status);
     setStatus(result.data.transaction.status);
     setWbtcSrcAmount(result.data.transaction.receivingAmount);
   }, [result]);
