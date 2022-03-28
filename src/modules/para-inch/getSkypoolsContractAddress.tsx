c1a2680ce3ef30aeb961e872f3f261a890b8592f
Agustín Longoni <agustin.longoni@altoros.com>
2022-03-28 09:51:28 -0300

    update the contract address (#47)

--- src/modules/para-inch/getSkypoolsContractAddress.tsx
+++ src/modules/para-inch/getSkypoolsContractAddress.tsx
@@ -3,7 +3,7 @@ export { swapMinAmount } from './swapMinAmount';
 
 const CONTRACT_SKYPOOLS = {
   ETHEREUM: '',
-  ROPSTEN: '0x7dfbf7E38F188Da1BD337A128ce7A5D758957621',
+  ROPSTEN: '0x92c95b6227a9f0b4602649bd83f83adc48dae903',
   BSC: '',
 };
 
