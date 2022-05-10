7fa9ff0ccf785c7afb84d7dd438b171a6ea1ad1a
Agustin Pane <agustin.pane@gmail.com>
2022-05-10 18:01:39 +0200

    Adds hasEnoughAllowance and requestAllowance return in useCreateSwap

--- src/modules/para-inch-react/useCreateSwap/index.tsx
+++ src/modules/para-inch-react/useCreateSwap/index.tsx
@@ -1,11 +1,14 @@
 import { Wallet } from 'bnc-onboard/dist/src/interfaces';
+import { useEffect, useState } from 'react';
+import Web3 from 'web3';
 
 import { useWalletConnection } from '../../hooks/useWalletConnection';
 import { useParaInchForm } from '../useParaInchForm';
 import { ParaInchContextValue } from '../context';
+import { checkTokenAllowance, increaseAllowance } from '../../web3';
 
 import { useCreateSkyPoolsSwap } from './useCreateSkyPoolsSwap';
-import { useCreateSwapAmongERC20S } from './useCreateSwapAmongERC20S';
+import { UseCreateSwapAmongERC20S, useCreateSwapAmongERC20S } from './useCreateSwapAmongERC20S';
 
 export type SwapReturn = {
   btcAddress: string;
@@ -19,6 +22,8 @@ export type SwapReturn = {
   minAmount: { amount: string; token: string };
   isEnoughDeposit: boolean;
   explorerLink: string;
+  hasEnoughAllowance: boolean;
+  requestAllowance: () => void;
 };
 
 export type UseCreateSwapsProps = {
@@ -36,6 +41,44 @@ export const useCreateSwap = (): SwapReturn & { isSkyPools: boolean; isFloatShor
   const isFromBtc = parainchValue.fromToken?.symbol === 'BTC';
   const isToBtc = parainchValue.toToken?.symbol === 'BTC';
   const isSkyPools = isToBtc || isFromBtc;
+  const [hasEnoughAllowance, setHasEnoughAllowance] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!wallet || !parainchValue.swapQuote || !address) {
+      return;
+    }
+
+    // @todo (agustin) check if peridocally using intervals
+    const verifyAllowance = async () => {
+      const web3 = new Web3(wallet.provider);
+      const { swapQuote } = parainchValue;
+
+      if (!swapQuote) {
+        return;
+      }
+
+      const enoughAllowance = await checkTokenAllowance(
+        swapQuote.srcToken.address,
+        swapQuote.bestRoute.spender,
+        address,
+        web3,
+      );
+      setHasEnoughAllowance(enoughAllowance);
+    };
+    verifyAllowance();
+  }, [wallet, parainchValue, address]);
+
+  const requestAllowance = async () => {
+    console.log('Requesting allowance..');
+    const { swapQuote } = parainchValue;
+    if (!swapQuote || !wallet || !address) {
+      return;
+    }
+
+    const web3 = new Web3(wallet.provider);
+
+    await increaseAllowance(swapQuote.srcToken.address, swapQuote.bestRoute.spender, address, web3);
+  };
 
   const skypoolsSwap = useCreateSkyPoolsSwap({
     isFromBtc,
@@ -58,14 +101,17 @@ export const useCreateSwap = (): SwapReturn & { isSkyPools: boolean; isFloatShor
   if (isSkyPools && skypoolsSwap) {
     return {
       isSkyPools,
-      isFloatShortage: skypoolsSwap.isFloatShortage,
-      ...(skypoolsSwap as SwapReturn),
+      hasEnoughAllowance,
+      requestAllowance,
+      ...skypoolsSwap,
     };
   }
 
   return {
     isSkyPools,
     isFloatShortage: false,
-    ...(erc20Swap as SwapReturn),
+    hasEnoughAllowance,
+    requestAllowance,
+    ...(erc20Swap as UseCreateSwapAmongERC20S),
   };
 };
