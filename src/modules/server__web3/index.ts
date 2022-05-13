316708ff29b24caba941e2dcf2718fd08c58227f
Agustin Pane <agustin.pane@gmail.com>
2022-05-13 15:47:30 +0200

    Move web3 utils to same folder

--- src/modules/server__web3/index.ts
+++ src/modules/server__web3/index.ts
@@ -8,7 +8,7 @@ import {
   server__infuraProjectId,
   server__infuraProjectSecret,
 } from '../server__env';
-import { getScanApiUrl } from '../web3';
+import { Erc20ABI } from '../abis/erc20';
 
 export const buildWeb3Instance = ({ network }: { network: Network }) => {
   const url = (() => {
@@ -52,5 +52,71 @@ const getScanApiKey = ({ network }: { network: Network }) => {
       return server__etherscanSecret;
     default:
       return undefined;
+  }
+};
+
+export const getScanApiUrl = (network: Network) => {
+  switch (network) {
+    case Network.ETHEREUM:
+      return 'https://api.etherscan.io/api';
+    case Network.ROPSTEN:
+      return 'https://api-ropsten.etherscan.io/api';
+    default:
+      throw new Error(`Cannot find a scan API endpoint for network "${network}"`);
+  }
+};
+
+export const increaseAllowance = async (
+  tokenAddress: string,
+  contractAddress: string,
+  userAddress: string,
+  allowance: number,
+  web3: Web3,
+): Promise<void> => {
+  const tokenContract = new web3.eth.Contract(Erc20ABI as any, tokenAddress);
+  await tokenContract.methods.increaseAllowance(contractAddress, allowance).send({
+    from: userAddress,
+  });
+};
+
+export const checkTokenAllowance = async (
+  tokenAddress: string,
+  contractAddress: string,
+  userAddress: string,
+  minAllowance: number,
+  web3: Web3,
+): Promise<boolean> => {
+  const tokenContract = new web3.eth.Contract(Erc20ABI as any, tokenAddress);
+
+  try {
+    const result = Number(
+      await tokenContract.methods.allowance(userAddress, contractAddress).call(),
+    );
+
+    if (!result || result < minAllowance) {
+      return false;
+    }
+  } catch (error) {
+    console.error('Allowance check failed', error);
+    return false;
+  }
+
+  return true;
+};
+
+export const buildLinkToTransaction = ({
+  network,
+  transactionHash,
+}: {
+  network: Network;
+  transactionHash: string;
+}) => {
+  switch (network) {
+    case Network.ETHEREUM:
+      return `https://etherscan.io/tx/${transactionHash}`;
+    case Network.ROPSTEN:
+      return `https://ropsten.etherscan.io/tx/${transactionHash}`;
+    default:
+      throw new Error(`Cannot find a transaction explorer for network "${network}"`);
   }
 };
