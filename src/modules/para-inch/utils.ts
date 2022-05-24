74c7d212ce4a45200a3bbdee09250a1085d2a132
Agustin Pane <agustin.pane@gmail.com>
2022-05-24 11:53:27 +0200

    Adds translation key for swap.loss-greater-than-max-impact

--- src/modules/para-inch/utils.ts
+++ src/modules/para-inch/utils.ts
@@ -1,5 +1,9 @@
 import { APIError } from 'paraswap';
 
+export enum ParaSwapErrorEnum {
+  ESTIMATED_LOSS_GREATER_THAN_MAX_IMPACT = 'ESTIMATED_LOSS_GREATER_THAN_MAX_IMPACT',
+}
+
 export const isParaSwapApiError = (value: any): value is APIError => {
   if (value?.message === 'ESTIMATED_LOSS_GREATER_THAN_MAX_IMPACT') {
     return false;
