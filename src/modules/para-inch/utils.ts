69415d38ff6ca9e7b1a830c5748e6e5cfadbdef7
Agustin Pane <agustin.pane@gmail.com>
2022-06-02 15:46:12 +0200

    Merge branch 'master' of https://github.com/SwingbyProtocol/skypools-app into staging
    # Conflicts:
    #	src/modules/para-inch-react/useCreateSwap/index.tsx
    #	src/modules/para-inch/utils.ts
    #	src/scenes/SwapScene/Widget/index.tsx
    #	src/scenes/SwapScene/Widget/styles.tsx

---
+++ src/modules/para-inch/utils.ts
@@ -0,0 +1,8 @@
+import { APIError } from 'paraswap';
+
+export const isParaSwapApiError = (value: any): value is APIError => {
+  if (value?.message === 'ESTIMATED_LOSS_GREATER_THAN_MAX_IMPACT') {
+    return false;
+  }
+  return typeof value === 'object' && typeof value.message === 'string';
+};
